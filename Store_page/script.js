edfb7e4e485396b93fe9b019f3c3c1c982dafd1a
Blake Bless and Patrick Wells <blake@>
2016-03-23 13:18:55 -0700

    made reciept machine

---
+++ Store_page/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+  $("form#add").submit(function(event){
+    event.preventDefault();
+    var Name = $("input#name").val();
+    var Email = $("input#email").val();
+    var Phone = $("input#phone-number").val();
+    var Address = $("input#billing-address").val();
+
+    $(".name").append(Name);
+    $(".email").append(Email);
+    $(".phone").append(Phone);
+    $(".address").append(Address);
+
+
+  });
+
+});
